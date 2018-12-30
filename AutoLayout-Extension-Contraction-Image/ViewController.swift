c42dc9d7e468590a71c7fd6e82a2b50b906bef9c
ddd503 <skbsporta7@gmail.com>
2018-12-30 14:44:30 +0900

    Initial Commit

---
+++ AutoLayout-Extension-Contraction-Image/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout-Extension-Contraction-Image
+//
+//  Created by kawaharadai on 2018/12/30.
+//  Copyright © 2018 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
